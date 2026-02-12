e775922a4a283a04f90176c72818242ef64ff03b
mm1053@student.le.ac.uk <129211156+MZMurphy@users.noreply.github.com>
2026-02-12 11:53:46 +0000

    AES Key GEN

--- WannaCry/src/le/ac/uk/WannaCry/WannaCry.java
+++ WannaCry/src/le/ac/uk/WannaCry/WannaCry.java
@@ -6,11 +6,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.SecureRandom;
+import java.security.KeyStore.SecretKeyEntry;
+import javax.crypto.SecretKey; // I added this for Secret Key? Not sure if that means the above may be the wrong lib.
+import java.security.SecureRandom; // For SecureRandom();
+import javax.crypto.KeyGenerator;
 
 public class WannaCry {
-    public static void main(String[] args) {
-        // TODO - Generate a random 256-bit AES key
-
+    public static void main(String[] args) throws Exception{
+        // Gen 256-bit AES key - Lab2. 
+        KeyGenerator Key_Generator = KeyGenerator.getInstance("AES");
+        Key_Generator.init(256, new SecureRandom());
+        SecretKey KEY_AES = Key_Generator.generateKey();
 
         byte[] fileBytes = null;
         try (FileInputStream fis = new FileInputStream("test.txt")) {
@@ -40,7 +47,10 @@ public class WannaCry {
             return;
         }
 
-        // TODO - Construct public key from masterPublicKeyBase64
+        // TO DO - Construct Public key  from masterPublicKeyBase64
+     
+        
+
         String masterPublicKeyBase64 = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqW9Skh563WZyyNnXOz3kK8QZpuZZ3rIwnFpPqoymMIiHlLBfvDKlHzw1xWFTqISBLkgjOCrDnFDy/LZo8hTFWdXoxoSHvZo/tzNkVNObjulneQTy8TXdtcdPxHDa5EKjXUTjseljPB8rgstU/ciFPb/sFTRWR0BPb0Sj0PDPE/zHW+mjVfK/3gDT+RNAdZpQr6w16YiQqtuRrQOQLqwqtt1Ak/Oz49QXaK74mO+6QGtyfIC28ZpIXv5vxYZ6fcnb1qbmaouf6RxvVLAHoX1eWi/s2Ykur2A0jho41GGXt0HVxEQouCxho46PERCUQT1LE1dZetfJ4WT3L7Z6Q6BYuQIDAQAB";
 
 

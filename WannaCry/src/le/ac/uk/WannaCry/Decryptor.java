af32cf0c6a1d1ff543b9653b0bc9924bbc4e21cf
mm1053@student.le.ac.uk <129211156+MZMurphy@users.noreply.github.com>
2026-02-16 11:13:08 +0000

    Decryptor.java AES key read, user priv key, gen sig, and decryption with AES. NOTE: I haven't been able to properly test due to server dependency!

--- WannaCry/src/le/ac/uk/WannaCry/Decryptor.java
+++ WannaCry/src/le/ac/uk/WannaCry/Decryptor.java
@@ -5,7 +5,13 @@ import java.net.Socket;
 import java.security.KeyFactory;
 import java.nio.file.Files;
 import java.security.PrivateKey;
+import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.nio.file.Path;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import java.io.File;
 
 public class Decryptor {
@@ -14,7 +20,6 @@ public class Decryptor {
         String port = args[1];
         String userid = args[2];
 
-        // TODO - Generate signature using userid and AES key as content with SHA256withRSA (and public or private key that identifies user?)
         byte[] decryptedAESKeyBytes = null; // what the server sends back
         byte[] Encrypted_AES_Key = null; // read from aes.key
 
@@ -29,7 +34,12 @@ public class Decryptor {
         KeyFactory Factory_Key = KeyFactory.getInstance("RSA");
         PrivateKey Key_Private = Factory_Key.generatePrivate(spec);
 
-
+        // From Lab 4. Signature generation.
+        Signature Our_Signature = Signature.getInstance("SHA256withRSA");
+        Our_Signature.initSign(Key_Private);
+        Our_Signature.update(userid.getBytes());
+        Our_Signature.update(Encrypted_AES_Key);
+        byte[] signature = Our_Signature.sign();
 
         try (Socket socket = new Socket(host, Integer.parseInt(port))) {
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
@@ -59,7 +69,22 @@ public class Decryptor {
             return;
         }
 
-        // TODO - Decrypt test.txt.cry using the decrypted AES key and write to test.txt
+        //Decrypt test.txt.cry using the decrypted AES key and write to test.txt. 
+
+        SecretKeySpec KEY_AES = new SecretKeySpec(decryptedAESKeyBytes, "AES");
+        IvParameterSpec Zero_Padding_IV = new IvParameterSpec(new byte[16]);
+        Cipher AES_Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        AES_Cipher.init(Cipher.DECRYPT_MODE, KEY_AES, Zero_Padding_IV);
+
+        // Read encrypted file and decrypt.
+        byte[] encryptedFile = Files.readAllBytes(Path.of("test.txt.cry"));
+        byte[] decryptedFile = AES_Cipher.doFinal(encryptedFile);
+        
+        // write to test.txt
+        try(FileOutputStream fos = new FileOutputStream("test.txt")){
+            fos.write(decryptedFile);
+        }
+
 
 
         String successMessage = """

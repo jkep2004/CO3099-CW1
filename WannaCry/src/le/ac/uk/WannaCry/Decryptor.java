def96b325356ae99eb3938326bbd570b844fcec2
jkep2004 <97905011+jkep2004@users.noreply.github.com>
2026-02-16 19:28:39 +0000

    Merge encryption into boilerplate (#1)
    * AES Key GEN
    
    * AES key gen, AES/CBC encryption, RSA public key from Base64, AES to aes.key. Also I changed the message for fun.
    
    * Read encrypted AES key fromm aes.key, loading teh private userkey from <userid>.prv and generated the SHA256withRSA sig
    
    * Decryptor.java AES key read, user priv key, gen sig, and decryption with AES. NOTE: I haven't been able to properly test due to server dependency!
    
    ---------
    
    Co-authored-by: mm1053@student.le.ac.uk <129211156+MZMurphy@users.noreply.github.com>

--- WannaCry/src/le/ac/uk/WannaCry/Decryptor.java
+++ WannaCry/src/le/ac/uk/WannaCry/Decryptor.java
@@ -1,16 +1,46 @@
 package le.ac.uk.WannaCry;
-
-import java.io.*;
+import java.io.*; // uncessary? not sure. 
 import java.net.Socket;
+// Zara Imports
+import java.security.KeyFactory;
+import java.nio.file.Files;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.nio.file.Path;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.File;
 
 public class Decryptor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         String host = args[0];
         String port = args[1];
         String userid = args[2];
 
-        // TODO - Generate signature using userid and AES key as content with SHA256withRSA (and public or private key that identifies user?)
-        byte[] decryptedAESKeyBytes = null;
+        byte[] decryptedAESKeyBytes = null; // what the server sends back
+        byte[] Encrypted_AES_Key = null; // read from aes.key
+
+        try(FileInputStream FIS = new FileInputStream("aes.key")){ // lab1
+             Encrypted_AES_Key = FIS.readAllBytes();
+        }
+
+        // Lab 3. 
+        File File_For_Key = new File(userid + ".prv");
+        byte[] Byte_From_Key = Files.readAllBytes(File_For_Key.toPath());
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(Byte_From_Key);
+        KeyFactory Factory_Key = KeyFactory.getInstance("RSA");
+        PrivateKey Key_Private = Factory_Key.generatePrivate(spec);
+
+        // From Lab 4. Signature generation.
+        Signature Our_Signature = Signature.getInstance("SHA256withRSA");
+        Our_Signature.initSign(Key_Private);
+        Our_Signature.update(userid.getBytes());
+        Our_Signature.update(Encrypted_AES_Key);
+        byte[] signature = Our_Signature.sign();
+
         try (Socket socket = new Socket(host, Integer.parseInt(port))) {
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
             DataInputStream dis = new DataInputStream(socket.getInputStream());
@@ -39,7 +69,22 @@ public class Decryptor {
             return;
         }
 
-        // TODO - Decrypt test.txt.cry using the decrypted AES key and write to test.txt
+        //Decrypt test.txt.cry using the decrypted AES key and write to test.txt. 
+
+        SecretKeySpec KEY_AES = new SecretKeySpec(decryptedAESKeyBytes, "AES");
+        IvParameterSpec Zero_Padding_IV = new IvParameterSpec(new byte[16]);
+        Cipher AES_Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        AES_Cipher.init(Cipher.DECRYPT_MODE, KEY_AES, Zero_Padding_IV);
+
+        // Read encrypted file and decrypt.
+        byte[] encryptedFile = Files.readAllBytes(Path.of("test.txt.cry"));
+        byte[] decryptedFile = AES_Cipher.doFinal(encryptedFile);
+        
+        // write to test.txt
+        try(FileOutputStream fos = new FileOutputStream("test.txt")){
+            fos.write(decryptedFile);
+        }
+
 
 
         String successMessage = """
